607c78847a1d33bf41a1cd8c2b8e37af21480b54
Elliott Chong <elliottchong16@gmail.com>
2022-06-26 21:38:58 +0800

    idk

--- sketch.js
+++ sketch.js
@@ -1,6 +1,25 @@
-let equation = [5, 2, 3];
+let equation = [1];
 // 5x^2 + 2x^1 + 3x^0
 
-let f = (x) => {
-  for (let i = 0; i < equation.length; i++) {}
+const f = (x) => {
+  let output = 0;
+  // 2-1-0   0-1-2
+  for (let i = equation.length - 1; i >= 0; i--) {
+    output += equation[Math.abs(i - equation.length + 1)] * x ** i;
+  }
+  return output;
 };
+
+function setup() {
+  createCanvas(800, 800).parent(select("#canvas"));
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  for (let x = -width / 2; x < width / 2; x++) {
+    let output = f(x);
+    output = map(output, f(-width / 2), f(width / 2), height, 0);
+    point(x, output);
+  }
+}
